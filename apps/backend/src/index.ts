315b7a4c98a9ac718b69f7fcec557a929c726352
Armando Rios <armandorios.dev@gmail.com>
2025-07-26 10:27:46 -0700

    feat(server): setup app routes

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,1 +1,22 @@
-console.log('Hello, World!');
+import express from 'express';
+import cors from 'cors';
+import * as dotenv from 'dotenv';
+import authRoutes from './features/auth/routes/auth';
+import { authMiddleware } from './shared/middleware/authMiddleware';
+import getRoutes from './features/greet/routes/greet';
+import { connectDB } from './shared/config/database';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+app.use('/api/auth', authRoutes);
+app.use(authMiddleware);
+app.use('/api/get', getRoutes);
+
+app.listen(PORT, () => console.log(`✅ Server is running on port ${PORT}`));
